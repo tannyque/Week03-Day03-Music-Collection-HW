a1d2eb675fc0635eb7731e7a088e846b67390eed
tannyque <tannyque1992@gmail.com>
2018-08-01 19:08:04 +0100

    Adds album class and creates save for albums

--- models/albums.rb
+++ models/albums.rb
@@ -1,2 +1,21 @@
 require('pg')
 require_relative("../db/sql_runner")
+
+class Album
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id)
+    VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i()
+  end
+  
+end
