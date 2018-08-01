8b0fead25a82978cbf04e9aeb55624986276e15a
tannyque <tannyque1992@gmail.com>
2018-08-01 20:40:06 +0100

    Adds update to artists

--- models/artists.rb
+++ models/artists.rb
@@ -32,4 +32,10 @@ class Artist
     return results.map{ |album| Album.new(album) }
   end
 
+  def update()
+    sql = "UPDATE artists SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
 end
